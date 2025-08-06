62ec5b9b536d92d3e5ff7018719c8ce69ca7c6a7
Abhijeet Dwivedi <abhijeetdwivedi627@gmail.com>
2025-08-07 01:54:54 +0530

    Prepare project for deployment and testing

--- src/app/services/website-redesign/page.jsx
+++ src/app/services/website-redesign/page.jsx
@@ -4,7 +4,7 @@ import MediaTextBlock from '@/components/MediaTextBlock';
 
 export default function WebsiteRedesign() {
     return (
-        <section className="my-4 px-4">
+        <section className="my-40 px-4">
             <div className="max-w-7xl mx-auto space-y-8 ">
                 <h2 className="massive2 text-center">{data.services.websiteRedesign.title}</h2>
                 <p className="text-center para2 mb-12">{data.services.websiteRedesign.description}</p>
